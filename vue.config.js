29537c1685fda824fa85213092bc0aeb0577b3f8
meng <21799626@qq.com>
2022-08-30 13:14:46 +0800

    mou13:14

--- vue.config.js
+++ vue.config.js
@@ -1,6 +1,6 @@
 module.exports = {
-    publicPath: './myvue',
+    publicPath: './',
     outputDir: "dist", // 输出文件目录
     lintOnSave: false, // eslint 是否在保存时检查
-    assetsDir: 'static', // 配置js、css静态资源二级目录的位置  
+    assetsDir: 'myvue', // 配置js、css静态资源二级目录的位置  
   }
